240d86b1d603c17c7f7010353fa59f98a66b3743
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-04 11:13:55 +0800

    Dive into spelling using text checker

--- WordScramble/TextCheckerView.swift
+++ WordScramble/TextCheckerView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct TextCheckerView: View {
     var body: some View {
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
         
         Text("Hello, World!")
     }

e2942e0c095ae8744eee2975eadc3577560a770a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-04 10:17:25 +0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Giovanni Gaffé on 2021/10/4.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

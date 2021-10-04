5c11d5c59460953b190bd35b8c1e1fc816e8a471
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-04 11:08:34 +0800

    Text checker file

---
+++ WordScramble/TextCheckerView.swift
@@ -0,0 +1,21 @@
+//
+//  TextCheckerView.swift
+//  WordScramble
+//
+//  Created by Giovanni Gaffé on 2021/10/4.
+//
+
+import SwiftUI
+
+struct TextCheckerView: View {
+    var body: some View {
+        
+        Text("Hello, World!")
+    }
+}
+
+struct TextCheckerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextCheckerView()
+    }
+}

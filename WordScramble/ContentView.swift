0070cfc08d3041d8abefbb137f7f81358117c3ce
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-06 16:33:39 +0800

    set row in one line

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -25,9 +25,11 @@ struct ContentView: View {
                     .autocapitalization(.none)
                     .padding()
                 
-                List(usedWords, id: \.self) {
-                    Image(systemName: "\($0.count).circle")
-                    Text($0)
+                List(usedWords, id: \.self) { word in
+                    HStack {
+                        Image(systemName: "\(word.count).circle")
+                        Text(word)
+                    }
                 }
                 
                 Text("Score : \(userScore) points").font(.headline)
